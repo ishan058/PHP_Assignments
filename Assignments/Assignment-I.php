94bfb9cc78670949528463560775da17c61334ec
ishan khadka <77224237+ishan058@users.noreply.github.com>
2022-08-10 00:20:47 +0545

    Add files via upload

---
+++ Assignments/Assignment-I.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Assignment-I</title>
+    </head>
+    <body>
+        <h1>Assignment-I</h1>
+        <?php
+            echo "Hello World. I am Ishan khadka."
+        ?>
+    </body>
+</html>
