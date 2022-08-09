01157fcc2e29ff2dcda6a20e9c8423f9090feaaa
ishan khadka <77224237+ishan058@users.noreply.github.com>
2022-08-09 22:57:11 +0545

    Add files via upload

--- Assignment-i.php
+++ Assignment-i.php
@@ -10,7 +10,17 @@
 			echo "Hello world!, I am Ishan Khadka";
 		?>
 		
-		<p><h1><marquee>THANK YOU!</marquee></h1></p>
+		<p><h1><marquee>
+			<span style="color:#FF0000">T</span>
+		    <span style="color:#66CC66">H</span>
+		    <span style="color:#FF9966">A</span>
+		    <span style="color:#FFCCCC">N</span>
+		    <span style="color:#FF0066">K</span>&nbsp;&nbsp;
+		    <span style="color:#66CC66">Y</span>
+		    <span style="color:#FF9966">O</span>
+		    <span style="color:#FFCCCC">U</span>
+		    <span style="color:#FF0066">!</span>
+		</marquee></h1></p>
 		
 	</body>
 </html>
